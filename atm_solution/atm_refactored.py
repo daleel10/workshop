93cb207b027a6d840ccd7a77a456bd2b3dc31d64
Abdirahman <daleel1439@gmail.com>
2018-09-17 10:54:21 +0300

    Change to the class/object style

--- atm_solution/atm_refactored.py
+++ atm_solution/atm_refactored.py
@@ -1,17 +1,33 @@
-def withdrawal(balance,request):
-    initial_req = request
-    # initialaze allowed papers
+class ATM():
+    # initialize valid papers
     allowed_papers = [100,50,20,10,5,4,3,2,1]
 
-    #check balance
-    if balance < request:
-        print('not enough')
-    for paper in range(len(allowed_papers)):
+    def __init__(self,balance,bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
+    def withdraw(self,request):
 
-        while request > 0 :
-            if request - allowed_papers[paper] >= 0 :
-                request -= allowed_papers[paper]
-                print("Give " + str(allowed_papers[paper]))
-            else:
-                break
-    return balance - initial_req
+
+        initial_req = request # in order to keep track the initial request/ this is usefull when we will update the balance
+        
+        print("Welcome to " + self.bank_name)
+        print("Current balance = " + str(self.balance))
+        print("=============================")
+        
+        if self.balance < request: 
+            print("Can't give you all this money !!")
+        else:
+            for paper in range(len(self.allowed_papers)):
+                while request > 0:
+                    # verify which paper you gave him
+                    if request - self.allowed_papers[paper] >= 0:
+                        request -= self.allowed_papers[paper]
+                        print("Give " + str(self.allowed_papers[paper]))
+                     
+                    else:
+                        # switch to the next paper/ exit while loop
+                        break
+        print("=============================")
+
+        # update balance to the new balance 
+        self.balance = self.balance - initial_req
